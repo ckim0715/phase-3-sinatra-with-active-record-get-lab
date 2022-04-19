6ec453efc26b6139646ad2ca4847a479d7741cee
Christopher <christopher.kim43@gmail.com>
2022-04-19 15:13:50 -0400

    commit tree 79478e54aecd0fda90a66035b7a5e1742fc07f19

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  get '/bakeries' do
+    bakeries = Bakery.all 
+    bakeries.to_json
+  end
 
+  get 'bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    baker.to_json
 end
